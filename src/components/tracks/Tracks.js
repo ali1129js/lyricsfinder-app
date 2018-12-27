5ee9ec725bb31a856e681283fa41abcd65da682f
ali1129js <ali1129js@gmail.com>
2018-12-27 10:14:43 +0100

    mapping the tracks list and bootstrap/FA

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -2,22 +2,32 @@
  * @Author: Ali
  * @Date:   2018-12-26T14:42:43+01:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-12-27T09:26:34+01:00
+ * @Last modified time: 2018-12-27T09:46:48+01:00
  */
 import React, { Component } from 'react'
 import { Consumer } from '../../context'
 import Spinner from '../layout/Spinner'
+import Track from './Track'
 class Tracks extends Component {
   render(){
     return (
       <Consumer>
         {value => {
-          const { track_list} = value
+          const { track_list, heading} = value
           console.log(value)
           if(track_list === undefined || track_list.length === 0){
             return <Spinner />
           } else {
-            return <h1> Tracks loaded </h1>
+            return (
+              <React.Fragment>
+                <h3 className="text-center mb-4"> {heading} </h3>
+                <div className="row">
+                  {track_list.map(item => (
+                    <Track key={item.track.track_id} track={item.track}/>
+                  ))}
+                </div>
+              </React.Fragment>
+            )
           }
         }}
       </Consumer>
