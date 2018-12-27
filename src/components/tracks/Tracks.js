d5aebf0da45b0e7323fce23ea067519799b5d1d7
ali1129js <ali1129js@gmail.com>
2018-12-27 09:28:11 +0100

    added spinner to handled empty res from the API

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -2,17 +2,23 @@
  * @Author: Ali
  * @Date:   2018-12-26T14:42:43+01:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-12-26T15:15:17+01:00
+ * @Last modified time: 2018-12-27T09:26:34+01:00
  */
 import React, { Component } from 'react'
 import { Consumer } from '../../context'
+import Spinner from '../layout/Spinner'
 class Tracks extends Component {
   render(){
     return (
       <Consumer>
         {value => {
+          const { track_list} = value
           console.log(value)
-          return <h1> Tracks </h1>
+          if(track_list === undefined || track_list.length === 0){
+            return <Spinner />
+          } else {
+            return <h1> Tracks loaded </h1>
+          }
         }}
       </Consumer>
     )
--- src/context.js
+++ src/context.js
@@ -2,18 +2,26 @@
  * @Author: Ali
  * @Date:   2018-12-26T14:51:02+01:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-12-26T15:16:27+01:00
+ * @Last modified time: 2018-12-27T09:19:19+01:00
  */
 import React , { Component } from 'react'
-
+import axios from 'axios'
 const Context = React.createContext()
 
 export class Provider extends Component {
   state = {
-    track_list:[
-      { track: {name:'abc'}}
-    ],
+    track_list:[],
     heading: 'Top 10 Tracks'
+  }
+  componentDidMount(){
+    axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=de&f_has_lyrics=1&apikey=${process.env.REACT_APP_API_KEY}`)
+    .then(res => {
+      console.log(res.data)
+      this.setState({
+        track_list:res.data.message.body.track_list
+      })
+    })
+    .catch(error => console.log(error))
   }
   render(){
     return (
