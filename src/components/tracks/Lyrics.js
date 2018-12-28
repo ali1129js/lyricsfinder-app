ebbd1a294301b6176358a35b2d4e7556046d783e
ali1129js <ali1129js@gmail.com>
2018-12-28 15:13:02 +0100

    lyrics component done

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -2,10 +2,12 @@
  * @Author: Ali
  * @Date:   2018-12-27T10:19:23+01:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-12-27T18:39:56+01:00
+ * @Last modified time: 2018-12-28T12:47:34+01:00
  */
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
+import Spinner from "../layout/Spinner";
 class Lyrics extends Component {
   state = {
     track: {},
@@ -34,11 +36,42 @@ class Lyrics extends Component {
   }
   render() {
     const { track, lyrics } = this.state;
-    return (
-      <div>
-        <h1> Lyrics </h1>
-      </div>
-    );
+    console.log(track);
+    if (
+      track === undefined ||
+      lyrics === undefined ||
+      Object.keys(track).length === 0 ||
+      Object.keys(lyrics).length === 0
+    ) {
+      return <Spinner />;
+    } else {
+      return (
+        <Fragment>
+          <Link to="/" className="btn btn-dark btn-sm mb-4">
+            Go Back
+          </Link>
+          <div className="card">
+            <h5 className="card-header">
+              {track.track_name} by{" "}
+              <span className="text-secondary"> {track.artist_name}</span>
+            </h5>
+            <div className="card-body">
+              <p className="card-text"> {lyrics.lyrics_body}</p>
+            </div>
+          </div>
+          <ul className="list-group mt-3">
+            <li className="list-group-item">
+              <strong> Album Name </strong>: {track.album_name}
+            </li>
+
+            <li className="list-group-item">
+              <strong> Explicit Words </strong>:{" "}
+              {track.explicit === 0 ? "No" : "Yes"}
+            </li>
+          </ul>
+        </Fragment>
+      );
+    }
   }
 }
 export default Lyrics;
