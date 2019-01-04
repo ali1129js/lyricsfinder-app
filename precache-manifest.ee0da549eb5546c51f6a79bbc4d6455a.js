6e08f417e1c86177a669b9f0f81819a7018305a8
ali1129js <ali1129js@gmail.com>
2019-01-04 14:26:12 +0100

    Updates

---
+++ precache-manifest.ee0da549eb5546c51f6a79bbc4d6455a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "74bbc625282f29cc72f8",
+    "url": "/lyricsfinder-app/static/js/runtime~main.74bbc625.js"
+  },
+  {
+    "revision": "ce7a007a47d56d4054d4",
+    "url": "/lyricsfinder-app/static/js/main.ce7a007a.chunk.js"
+  },
+  {
+    "revision": "3773fa3703f5383c6cd5",
+    "url": "/lyricsfinder-app/static/js/1.3773fa37.chunk.js"
+  },
+  {
+    "revision": "ce7a007a47d56d4054d4",
+    "url": "/lyricsfinder-app/static/css/main.99ddcba5.chunk.css"
+  },
+  {
+    "revision": "3773fa3703f5383c6cd5",
+    "url": "/lyricsfinder-app/static/css/1.c3659dee.chunk.css"
+  },
+  {
+    "revision": "ceb003967e3b51d178c8d9bbcb6f18eb",
+    "url": "/lyricsfinder-app/index.html"
+  }
+];
